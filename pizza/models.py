75571238b6b06b4f64ff396c40fdf1c630945cac
TyLOOIOIOOI <Ty_Liu1@baylor.edu>
2022-05-07 22:10:21 -0500

    comments

--- pizza/models.py
+++ pizza/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+#from pizza.views import Comment
+
 # Create your models here.
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=200)
@@ -9,6 +11,22 @@ class Pizza(models.Model):
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     topping_name = models.CharField(max_length=200)
+
+
+'''
+class Entry(models.Model):
+    topic = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        verbose_name_plural = 'entries'
+
+
+    def __str__(self): 
+        return f"{self.text[:50]}..."
+
+'''
 
 
 """
